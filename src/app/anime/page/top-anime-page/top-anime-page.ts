c7308c8c004139c1583d96b339e081bdabf6b671
Rinriku69 <siripoohkim@gmail.com>
2026-03-25 16:12:03 +0700

    fix: remove search

--- src/app/anime/page/top-anime-page/top-anime-page.ts
+++ src/app/anime/page/top-anime-page/top-anime-page.ts
@@ -1,5 +1,5 @@
-import { Component, ChangeDetectionStrategy, computed, input, inject, linkedSignal } from '@angular/core';
-import { topAnimeResource } from '../../helpers/resource';
+import { Component, ChangeDetectionStrategy, computed, input, inject, linkedSignal, effect } from '@angular/core';
+import { animeListResource, topAnimeResource } from '../../helpers/resource';
 import { AnimeListParams } from '../../types';
 import { ListAnime } from '../../components/list-anime/list-anime';
 import { Router, RouterLink } from '@angular/router';
@@ -17,36 +17,13 @@ import { purnEmptyProperties } from '../../helpers/utils';
 export class TopAnimePage {
   readonly q = input<string>();
   readonly page = input<string>();
-  private readonly router = inject(Router)
   private readonly params = computed<AnimeListParams>(() => ({
-    q: this.q() ?? '',
     page: this.page() ?? ''
   }))
 
   readonly resource = topAnimeResource(() => this.params());
 
 
-  protected readonly form = form(linkedSignal(() => ({ q: this.q() ?? '' })),
-    (path) => {
-      disabled(path, () => this.resource.isLoading())
-    })
-
-
-  protected onSearch(): void {
-    submit(
-      this.form,
-      async (form) =>
-        void this.router.navigate([], {
-          queryParams: purnEmptyProperties(form().value()),
-          replaceUrl: true
-        })
-    )
-  }
-
-  protected clearSearch(): void {
-    this.form.q().value.set('');
-    this.onSearch()
-  }
 
   protected readonly currentPage = computed(() => Number(this.page() ?? 1));
 
@@ -69,5 +46,6 @@ export class TopAnimePage {
       ? this.resource.value().pagination.current_page + 1
       : null,
   );
+
 
 }
